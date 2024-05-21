596920340819436df8c67195bcd672abacbe9fc1
anthony abbes <anthony-abbes@live.fr>
2024-05-21 10:08:11 +0200

    création du formulaire et du controlleur

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,134 +1,66 @@
 <?php
 
 require_once './src/Model/Common/Security.php';
-require_once './src/Model/Common/Regenerate.php';
-require_once './src/Model/Animaux.php';
-require_once './src/Model/Habitat.php';
-require_once './src/Model/Service.php';
-require_once './src/Model/RapportVeterinaire.php';
-
-
-
+require_once './src/Model/Common/SendMail.php';
 
 
 class ContactController
 {
     private $Security;
-    private $Animaux;
-    private $Habitat;
-    private $Service;
-    private $RapportVeterinaire;
+    private $SendMail;
 
 
-
-
-    private $Regenerate;
-
-    public function __construct(){
+    public function __construct()
+    {
         $this->Security = new Security();
-        $this->Regenerate = new Regenerate();
-        $this->Animaux = new Animaux();
-        $this->Habitat = new Habitat();
-        $this->Service = new Service();
-        $this->RapportVeterinaire = new RapportVeterinaire();
     }
 
-    public function contactPage(){
+    public function contactPage()
+    {
         $loader = new Twig\Loader\FilesystemLoader('./src/Templates');
         $twig = new Twig\Environment($loader);
+
+        // On récupère le résultat d'envoi du form s'il existe
+        if (isset($_SESSION['resultat']) and !empty($_SESSION['resultat'])) {
+            $res = $this->Security->filter_form($_SESSION['resultat']);
+
+            // Efface les résultats de la session pour éviter de les conserver plus longtemps que nécessaire
+            unset($_SESSION['resultat']);
+
+        } else {
+
+        $res = '';
+
+        }
+
+
         $template = $twig->load('contactPage.twig');
-        echo  $template->render([
+        echo $template->render([
             'base_url' => BASE_URL,
+            'res' => $res
         ]);
     }
 
-    
-    public function animalByIdPage(){
-        //récupération et envoi d'un habitat selon son id
+    public function contactForm()
+    {
+        //récupération et envoi du contenu du formulaire par email
 
-        (isset($_GET['animal'])) ? $animalId = $this->Security->filter_form($_GET['animal']) : $animalId = '';
-        $res = $this->Animaux->getByAnimalId($animalId);
-
-        
-        $loader = new Twig\Loader\FilesystemLoader('./src/Templates');
-        $twig = new Twig\Environment($loader);
-        $template = $twig->load('animalByIdPage.twig');
-        echo  $template->render([
-            'base_url' => BASE_URL,
-            'res' => $res
-        ]);    
-    }
+        (isset($_POST['Nom'])) ? $nom = $this->Security->filter_form($_POST['Nom']) : $nom = '';
+        (isset($_POST['email'])) ? $email = $this->Security->filter_form($_POST['email']) : $email = '';
+        (isset($_POST['message'])) ? $message = $this->Security->filter_form($_POST['message']) : $message = '';
 
 
-    public function createBddProd(){
-        
-        // Création de la base de données prod
-        if ($this->Regenerate->regenerateSqlProd('./src/Data/prod.sql')) {
-            echo 'la base de données a été créée';
-        }else{
-            echo 'une erreur est survenue, consultez les logs';
+        $send = new SendMail($this->Security->filter_form($_ENV['FROM']), $this->Security->filter_form($_ENV['FROM']), $this->Security->filter_form($nom), $this->Security->filter_form($email), $this->Security->filter_form($message));
+        if ($send->mailSend()) {
+            $res = "Votre demande est bien transmise";
+        } else {
+            $res = "Echec de l'envoi du message";
         }
+        // Stockage des résultats dans la session puis redirection pour éviter renvoi au rafraichissement
+        $_SESSION['resultat'] = $res;
+        
+        header('Location: ' . BASE_URL . 'contact');
+        exit;
     }
 
-    public function createBddTest(){
-        // Importation des variables d'environnement test
-        require_once './src/Config/envTest.php'; 
-        // Création de la base de données TEST
-        if ($this->Regenerate->regenerateSqlProd('./tests/Data/testprod.sql')) {
-            echo 'la base de données a été créée';
-        }else{
-            echo 'une erreur est survenue, consultez les logs';
-        }
-    }
-
-    public function apiGetImgAnimaux(){
-
-        //récupération et envoi des 1ere images animaux en json
-        //On veut les images des 15 premiers animaux afin d'alléger la page
-        $res = $this->Animaux->getRandomActiveAnimauxNamesWithFirstImg(15);
-        Model::sendJSON($res) ;     
-    }
-
-    public function apiGetAnimauxByHabitat(){
-        //récupération et envoi des animaux d'un habitat en json
-        (isset($_GET['habitat'])) ? $habitatId = $this->Security->filter_form($_GET['habitat']) : $habitatId = '';
-        $res = $this->Animaux->getActiveAnimauxNamesByHabitat($habitatId);
-        Model::sendJSON($res) ;     
-    }
-
-    public function apiGetAnimauxByIdAnimal(){
-        //récupération et envoi de l'animal selon un id en json
-        (isset($_GET['animal'])) ? $animalId = $this->Security->filter_form($_GET['animal']) : $animalId = '';
-        $res = $this->Animaux->getByAnimalId($animalId);
-        Model::sendJSON($res) ;     
-    }
-
-    public function apiGetRapportByIdAnimal(){
-        //récupération et envoi des rapports vétérinaires selon un id animalen json
-        (isset($_GET['animal'])) ? $animalId = $this->Security->filter_form($_GET['animal']) : $animalId = '';
-        $res = $this->RapportVeterinaire->getByRapportByIdAnimal($animalId);
-        Model::sendJSON($res) ;     
-    }
-
-    public function apiGetImgHabitats(){
-
-        //récupération et envoi des 1ere images habitats en json
-        $res = $this->Habitat->getAllHabitatsNamesWithFirstImg();
-        Model::sendJSON($res) ;     
-    }
-
-    public function apiGetAllHabitats(){        
-        //récupération et envoi des habitats en json
-
-        $res = $this->Habitat->getAllHabitatNames();
-        Model::sendJSON($res) ;     
-    }
-
-
-    public function apiGetServices(){
-
-        //récupération et envoi des services en json
-        $res = $this->Service->getAllServiceNames();
-        Model::sendJSON($res) ;     
-    }
 }
